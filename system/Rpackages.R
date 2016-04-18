58e05c57f5121079824f0f7a3474424c03bddec6
Jeremy Raw <jeremy@nortonraw.com>
2016-04-18 11:11:30 -0400

    Fixed up R package installation so as not to reinstall already-installed packages; also fixed a race condition in Rserve.init

--- system/Rpackages.R
+++ system/Rpackages.R
@@ -1,2 +1,6 @@
-install.packages(c("sp","raster","rgdal","gdistance","Rserve","RSclient"), repos='https://mirrors.nics.utk.edu/cran/', lib='/usr/local/lib/R/site-library')
-
+package.list <- c("sp","raster","rgdal","gdistance","Rserve","RSclient")
+for (p in package.list) {
+    if (! require(p,character.only=T)) {
+        install.packages(p,lib='/usr/local/lib/R/site-library')
+    }
+}
