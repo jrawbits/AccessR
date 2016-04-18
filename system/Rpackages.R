e151c94a1158e5bd1c681a16842d1c8878be781c
Jeremy Raw <jeremy@nortonraw.com>
2016-04-18 12:30:37 -0400

    Fixed missing repository in package installation

--- system/Rpackages.R
+++ system/Rpackages.R
@@ -1,6 +1,6 @@
 package.list <- c("sp","raster","rgdal","gdistance","Rserve","RSclient")
 for (p in package.list) {
     if (! require(p,character.only=T)) {
-        install.packages(p,lib='/usr/local/lib/R/site-library')
+        install.packages(p,lib='/usr/local/lib/R/site-library',repos='https://mirrors.nics.utk.edu/cran/')
     }
 }
